2ba2ad02a212232001dbc711676ed0a3cb3eff83
javadev797982 <javadev797982@gmail.com>
2024-01-07 22:17:34 +0530

    131:Create save post feature

---
+++ src/main/java/com/myblog003/myblog003/Myblog003Application.java
@@ -0,0 +1,13 @@
+package com.myblog003.myblog003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog003Application.class, args);
+	}
+
+}
